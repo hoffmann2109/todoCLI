bb0ea626e7674e52c82ae09fd6710ebcd524aa14
Thomas Hoffmann <t.hoffmann2109@gmx.at>
2025-07-01 14:26:33 +0200

    Added .env and changed gitignore for the DB key

--- CLI_TODO/Program.cs
+++ CLI_TODO/Program.cs
@@ -1,7 +1,12 @@
 ﻿using CLI_TODO.Logic.InputOutput;
+using DotNetEnv;
+
+Env.Load();
 
 var outputService = new OutputService();
 var inputService = new InputService();
+var conn = Environment.GetEnvironmentVariable("MONGO_CONN")
+           ?? throw new InvalidOperationException("Set MONGO_CONN in .env");
 
 outputService.PrintWelcomeMessage();
 
